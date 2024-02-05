f08440b5004b6df4dc4b55f4c96a9e73777b4a7e
meamitverma <amit.verma.22080@gmail.com>
2024-02-05 12:33:37 +0530

    functional composition

--- ModernCPP/function_pointer/functoin_pointer.cpp
+++ ModernCPP/function_pointer/functoin_pointer.cpp
@@ -10,10 +10,19 @@ constexpr auto add(const int a, const int b) -> int {
 // Place holder or Callback.
 int (*MyFnPtr)(int x, int y) = add;
 
+// passing function reference as argument
+// functional composition
+int resultOfCalculation(int (*OP)(int, int), int x, int y) {
+    return  (*OP)(x, y);
+}
 
 int main() {
 
+    // outdated way of calling
     std::cout << "Sum of funptr = " << MyFnPtr(12,34) << std::endl;
+    
+    int res = resultOfCalculation(add, 2, 5);
+    std::cout << res << std::endl;
     
     return 0;
 }
