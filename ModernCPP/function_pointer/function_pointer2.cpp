14511144de79b4472d7177188b48d2cca9990e7b
meamitverma <amit.verma.22080@gmail.com>
2024-02-05 16:07:34 +0530

    template

---
+++ ModernCPP/function_pointer/function_pointer2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<type_traits>
+
+int main() {
+    // using namespace std::placeholders;
+    // std::function< int(int) > std_fun = std::bind(&auto, 123, 344);
+    // itn
+
+    return 0;
+}
---
+++ ModernCPP/function_pointer/typename.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<type_traits>
+#include<cstdio>
+
+template<typename T, typename... Ts>
+constexpr inline bool are_same_v = 
+    std::conjunction_v<std::is_same<T, Ts>...>;
+
+template<typename T, typename...>
+struct first_arg {
+    using type = T;
+};
+
+template<typename... Args>
+using first_arg_t = typename first_arg<Args...>::type;
+
+template<typename... Args>
+std::enable_if_t< are_same_v<Args...>, first_arg_t<Args...> >
+Add(const Args&... args) noexcept {
+    return (... + args);
+}
+
+
+#ifdef WILL_NOT_COMPILE
+void willnotCompile() {
+    printf("Main", Add(2, 3, 4.9));
+}
+
+#endif
+
+int main() {
+    printf("%d\n", Add(2,3,4));
+    printf("%d\n", Add(2));
+
+    return 0;
+}
