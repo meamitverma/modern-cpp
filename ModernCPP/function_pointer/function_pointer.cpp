b570373f7174a269bfeefe0b4e41aef814ca3686
meamitverma <amit.verma.22080@gmail.com>
2024-02-05 13:34:10 +0530

    update

--- ModernCPP/function_pointer/function_pointer.cpp
+++ ModernCPP/function_pointer/function_pointer.cpp
@@ -31,20 +31,21 @@ constexpr auto resultOfCalculation(
 }
 
 // third version
-template< typename T >
-struct is_variable : std::is_object<T> {};
+template <typename T>
+struct is_variable : std::is_object<T>
+{
+};
 
 template <typename FN, typename T1, typename T2>
 constexpr auto resultOfCalculation_3(
     const FN &OP,
     const T1 &x,
-    const T2 y) -> std::enable_if_t<
-                                    std::is_invocable_v<FN, T1, T2> &&  // check if this is a function
-                                    is_variable <T1> :: value && 
-                                    is_variable<T2> :: value,
+    const T2 y) -> std::enable_if_t<std::is_invocable_v<FN, T1, T2> && // check if this is a function
+                                        is_variable<T1>::value &&
+                                        is_variable<T2>::value,
 
                                     decltype((*OP)(x, y))
-                                    
+
                                     >
 { // to check if it is a function or not -> invokable checks for if the passed argument is a function or not and not a random pointer of a variable
 
