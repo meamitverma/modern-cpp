ecbebc750e1356e8f3f6e4fc097d5c96f65c423e
meamitverma <amit.verma.22080@gmail.com>
2024-02-08 11:51:35 +0530

    mvc framework

---
+++ CodeDesign/MVC/mvc_demo.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+
+// Model
+class CounterModel {
+
+    public:
+    // constructor
+    CounterModel() : count (0) {}
+
+    // getter
+    constexpr int getCount() const {
+        return count;
+    }
+
+    void increment() {
+        count++;
+    }
+
+    private:
+    int count;
+};
+
+// View -> only to display the data
+class CounterView{
+    public:
+    void displayCount(int count) const {
+        std::cout << "Count : " << count << std::endl;
+    }
+};
+
+
+// Controller
+class CounterController {
+    public:
+    CounterController (CounterModel &model,  CounterView &view) : model(model), view(view) {}
+
+    void incrementCounter() {
+        model.increment();
+    }
+
+    void updateViews() const {
+        view.displayCount( model.getCount() );
+    }
+
+    private:
+    CounterModel model;
+    CounterView view;   
+};
+
+int main() {
+
+    std::cout << "MVC Framework\n";
+
+    CounterModel model;
+    CounterView view;
+    CounterController controller(model, view);
+
+    // Initial display
+    controller.updateViews();
+    controller.incrementCounter();
+    controller.incrementCounter();
+    controller.incrementCounter();
+
+
+    // Display the updated view
+    controller.updateViews();
+
+    return 0;
+}
