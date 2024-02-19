b90f9532fdfde439377c49ddf4d5bc2af2321325
meamitverma <amit.verma.22080@gmail.com>
2024-02-19 13:25:37 +0530

    package task

---
+++ ModernCPP/WEEK3/feb19/package_task.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<future> // async threads, promise
+#include<thread>
+
+void increment () {
+    for (int i = 0; i < 100000; i++) {
+        for (int i1 = 0; i1 < 100000; i1++) {
+
+        }
+    }
+}
+
+// int() -> function pointer
+// anything given to packaged_task converted to asynchronous task
+// we use std::packaged_task, which can be used to wrap any target called as async calls
+std::packaged_task< int() > task ( []() {
+    std::this_thread::sleep_for(std::chrono::seconds( 3 ));
+    return 123;
+});
+
+
+int main() {
+
+    std::future< int > result = task.get_future();
+    std::thread( std::move(task)).detach();
+    std::cout << "Waiting" << std::endl;
+    result.wait();
+    std::cout << "Done!" << " Future result is " << result.get() << std::endl;
+    
+    return 0;
+}
