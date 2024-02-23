74fbe6c7e42042aeabadc0b1c6df48e821f26409
meamitverma <amit.verma.22080@gmail.com>
2024-02-23 12:59:45 +0530

    update

--- CodeMarathon/Final/ParallelExecution.cpp
+++ CodeMarathon/Final/ParallelExecution.cpp
@@ -1,44 +1,109 @@
 #include<iostream>
 #include<vector>
+#include<future>
+#include<functional>
 #include<algorithm>
-#include<chrono>
 #include<execution>
 
-int numElems = 1000;
-
-auto execute(const std::string &msg) {
-    return [ msg ]()
-    {
-        std::cout << msg <<std::endl;
-    };
+void execute(const std::string &msg) {
+    std:: cout <<  msg << std::endl;
 } 
 
 int main () {
-    std::vector<int> nums = {5, 2, 4 ,1, 3};
-    for (int i = 0; i < numElems; ++i) {
-        nums.emplace_back(i * 10);
-    }
-    
-    std::cout << "Parallel Execution" << std::endl;
+    std::vector<int> nums = {1, 2, 3, 4, 5};
 
-    // Start the timer
-    auto startTime = std::chrono::high_resolution_clock::now();
+    std::for_each(std::execution::par, nums.begin(), nums.end(), [](int &num){
+        execute("Thread " + std::to_string(num));
+    });
 
-    std::for_each(std::execution::par, nums.begin(), nums.end(), [](int &num){num *= 2;});
+    std::future<void> asyncThread = std::async(std::launch::async, execute, "thread");
+    asyncThread.wait();
 
-    // Stop the timer
-    auto endTime = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds> (endTime - startTime);
 
-    // Print the modified vector
-    std::cout << "Modified vector: ";
-    for (int num : nums) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
-    // Print the execution time
-    std::cout << "Execution time: " << duration.count() << " microseconds." << std::endl;
-    
     return 0;   
 }
+
+
+
+
+// write a lambda code to demonstrate the use of parallel execution using higher order functinos with an async thread
+
+
+
+/*
+
+#include <iostream>
+#include<future>
+
+using namespace std::chrono;
+
+typedef long ull;
+
+ull EvenSum = 0;
+ull OddSum = 0;
+
+ull findEven(ull start, ull end) {
+    ull EvenSum = 0;
+
+    std::cout << "Find Even Async Thread ID: " << std::this_thread::get_id() << std::endl; 
+
+    for (ull i = start; i <= end; i++) {
+        if (i % 2) {
+            EvenSum += i;
+        }
+    }
+
+    return EvenSum;
+}
+
+ull findODD(ull start, ull end) {
+    ull OddSum = 0;
+
+    std::cout << "Find Odd Async Thread ID: " << std::this_thread::get_id() << std::endl; 
+
+    for (ull i = start; i <= end; i++) {
+        if (!(i % 2)) {
+            OddSum += i;
+        }
+    }
+
+    return OddSum;
+}
+
+
+int main()
+{
+    std::cout << "Async Deferred threads..\n" << std::endl;
+    ull start = 0, end = 1900000000;
+
+    // Co routines.. Faster..
+    auto startTime = high_resolution_clock::now();
+
+    std::cout << "Async Thread in Main ID:" << std::this_thread::get_id() << std::endl;
+
+    // ------------------code starts
+        // std::future<ull> EvenSum = std::async(std::launch::async, findEven, start, end);
+        // std::future<ull> EvenOdd = std::async(std::launch::async, findODD, start, end);
+
+        std::future<ull> EvenSum = std::async(std::launch::deferred, findEven, start, end);
+        std::future<ull> EvenOdd = std::async(std::launch::deferred, findODD, start, end);
+    // ------------------code ends
+
+    std::cout << "Just for fun..." << std::endl;
+
+    std::cout << "Even sum : " << EvenSum.get() << std::endl;
+    std::cout << "Odd sum : " << EvenOdd.get() << std::endl;
+    std::cout << "Job done... " << std::endl;
+
+
+    auto stopTime = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stopTime - startTime);
+
+    std::cout << "Sec: " << duration.count() / 1000000 << std::endl;
+
+    return 0;
+}
+
+
+
+*/
